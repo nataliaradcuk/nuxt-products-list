4b78dc51e16f4da030cb1ce11b64729b9be8ed93
nataliradcuk <nataliaradcuk68@gmail.com>
2025-04-11 19:26:01 +0300

    feat: add product list page with pagination, search, and table data from API

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  compatibilityDate: '2024-11-04',
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui', '@vueuse/nuxt'],
+  css: ['~/assets/css/main.css'],
+  future: {
+    compatibilityVersion: 4
+  }
 })
